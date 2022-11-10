5672d6210b9e11f2426a7683dce5a0f669665500
Thomas Ripp <TRipp@labq.com>
2022-11-10 16:28:02 -0500

    404 page tweaks

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import './PageNotFound.css';
+import { Fragment } from "react";
+import "./PageNotFound.css";
 
 const PageNotFound = () => {
-    return (
-        <div className='container-lg text-center pt-5 pb-5 mb-4 mt-5'>
-            <h1 className='title-404 heading'>404</h1>
-            <img src='assets/img/404/hanginthere-landscape.jpg' className='undertale-img' />
-            <h2>Uh oh! You found a page that doesn't exist!</h2>
-            <h4 className='message-404'>Don't panic. Return to the Home Page to resume your journey towards a healthy life.</h4>
-            <a href='/' className='btn btn-return'>Return to Home</a>
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      <div className="banner">
+        <h1 className="title-404 heading">404</h1>
+      </div>
+      <div className="container-lg text-center mb-5 pb-5">
+        <img
+          src="assets/img/404/animation_640_labah5mk.gif"
+          className="img-404"
+        />
+        <h2>It seems this page hasn't been made yet!</h2>
+        <h4 className="message-404">
+          Return to the homepage to learn more about WholeQ from our current
+          information provided.
+        </h4>
+        <a href="/" className="btn btn-return">
+          Return to Home
+        </a>
+      </div>
+    </Fragment>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
